683b65f498cb166458417b0dab0dd6a2a459f4ec
Muhammad Zaman <33923090+zamansandhu@users.noreply.github.com>
2021-10-12 15:18:57 +0500

    Update settings.py

--- searchengine/settings.py
+++ searchengine/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_w16chxqdacx+zk(_h$-0_s9@4fcgq0-it^vkh3=@lxr&@7wt*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
